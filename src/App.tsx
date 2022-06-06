969c1df0a9a81a075fca46858e4648bdd57f67a8
Duc Thai <ducthai060501@gmail.com>
2022-06-07 00:43:42 +0300

    creat react app and fetch demo data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+  const body =  { "query": `{
+    stop(id: \"HSL:1040129\") {
+      name
+      lat
+      lon
+      wheelchairBoarding
+    }
+  }` }
+  const fetchData = async () => {
+    try {
+      const response = await axios.post(`https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql`, body,config)
+      console.log(response.data);
+    } catch (e) {
+      console.error(e);
+    }
+ }
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  
+ 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
 export default App;
+
+
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TodoList:React.FC = () => {
+    const todos = [{
+        id: 't1',
+        text: 'finish the text',
+    }]
+  return (
+    <ul>
+        {
+            todos.map(todo => (
+                <li key = {todo.id}>{todo.text}</li>
+            ))
+        }
+    </ul>
+  )
+}
+
+export default TodoList
